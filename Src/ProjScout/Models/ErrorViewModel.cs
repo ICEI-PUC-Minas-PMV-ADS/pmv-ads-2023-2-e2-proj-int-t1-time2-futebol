9e057d638fff82b77e607a27c3f90b5857ef374b
luizfsc <127213695+luizfsc@users.noreply.github.com>
2023-10-21 13:01:14 -0300

    Delete Src/ProjScout directory

--- Src/ProjScout/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjScout.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
